9b97e1f93b50dac49b1d42a156c1d7c592b574d6
khukuri-coder <shresthasabeeneng016@gmail.com>
2024-09-29 19:54:51 -0400

    set up in router

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,14 @@ import {
   loginUser,
   logoutUser,
   refreshAccessToken,
+  getCurrentUser,
+  changecurrentPassword,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateCoverImage,
+  updateUserAvatar,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -28,5 +36,16 @@ router.route("/login").post(loginUser);
 //secuedRoutes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("./refresh-token").post(refreshAccessToken);
+router.route("./change-password").post(verifyJWT, changecurrentPassword);
+router.route("./current-user").get(verifyJWT, getCurrentUser);
+router.route("./update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("./avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("./cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("./history").get(verifyJWT, getWatchHistory);
 
 export default router;
